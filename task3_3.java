eacc98dfe53ff718edcf901f691175b7551694f8
VladimirAlenkin <121186688+VladimirAlenkin@users.noreply.github.com>
2023-05-06 09:26:24 +0500

    Update task3_3.java

--- task3_3.java
+++ task3_3.java
@@ -15,12 +15,10 @@ public class task3_3 {
             return buffer1;
         }
 
-        //уже отсортирован
         int middle = startIndex + (endIndex - startIndex) / 2;
         int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
         int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
 
-        //слияние
         int index1 = startIndex;
         int index2 = middle;
         int destIndex = startIndex;
